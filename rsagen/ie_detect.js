c0b83bcd952366c0a7e95d0dc44d4fe246b6c010
rfl890 <87506407+rfl890@users.noreply.github.com>
2022-05-25 12:50:30 -0400

    Update ie_detect.js

--- rsagen/ie_detect.js
+++ rsagen/ie_detect.js
@@ -2,7 +2,7 @@
 
 (function () {
   if (navigator.userAgent.indexOf("MSIE") > 0) {
-    alert("Internet Explorer is not supported. Please, do yourself a favour and get yourself a newer browser.");
+    alert("Internet Explorer is not supported. You can use the IE version at https://rfl890.github.io/rsagen-ie.html");
   } else {
     return;
   }
